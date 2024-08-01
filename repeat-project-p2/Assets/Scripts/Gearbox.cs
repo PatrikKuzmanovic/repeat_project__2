99b6bb878b82bd0dc196c62d5cab3a50dd869b92
PatrikKuzmanovic <demond424@gmail.com>
2024-08-01 22:04:08 +0100

    Making_A_Car_Reverse_And_A_Reverse_Gear
    You can now reverse with "S" Key and it shows a letter "R" where Gear is to represent "Reverse"

--- repeat-project-p2/Assets/Scripts/Gearbox.cs
+++ repeat-project-p2/Assets/Scripts/Gearbox.cs
@@ -19,26 +19,35 @@ public class Gearbox : MonoBehaviour
     void Update()
     {
         float speed = carRigidbody.velocity.magnitude;
-
         speedKmh = speed * 3.6f;
 
-        UpdateGear();
+        bool isReversing = Vector3.Dot(carRigidbody.velocity, transform.forward) < 0;
+
+        UpdateGear(isReversing);
     }
 
-    void UpdateGear()
-    {
-        if (speedKmh < 20)
-            currentGear = 1;
-        else if (speedKmh < 45)
-            currentGear = 2;
-        else if (speedKmh < 70)
-            currentGear = 3;
-        else if (speedKmh < 95)
-            currentGear = 4;
-        else if (speedKmh < 120)
-            currentGear = 5;
-        else
-            currentGear = 6;
+    void UpdateGear(bool isReversing)
+    { 
+        if (isReversing)
+        {
+            currentGear = 0;
+        }
+        else 
+        {
+            if (speedKmh < 20)
+                currentGear = 1;
+            else if (speedKmh < 45)
+                currentGear = 2;
+            else if (speedKmh < 70)
+                currentGear = 3;
+            else if (speedKmh < 95)
+                currentGear = 4;
+            else if (speedKmh < 120)
+                currentGear = 5;
+            else
+                currentGear = 6;
+        }
+        
 
         UpdateGearText();
     }
@@ -47,6 +56,9 @@ public class Gearbox : MonoBehaviour
     {
         if (gearText != null)
         {
+            if (currentGear == 0)
+                gearText.text = "Gear: R";
+            else
             gearText.text = "Gear: " + currentGear;
         }
     }
